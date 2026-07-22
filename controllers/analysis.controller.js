83bcd918425d90b7e1a18cd8406e0c7ae1f55ed8
Aditya Agarwala <adityaagarwala111@gmail.com>
2026-07-22 11:49:49 +0530

    Migrate local Postgres to NeonDB

--- controllers/analysis.controller.js
+++ controllers/analysis.controller.js
@@ -18,6 +18,7 @@ export async function analyzeUser(req, res){
         const parsedInsights = JSON.parse(cleanedInsights);
         res.json({
             profile,
+            stats: analysis,
             insights: parsedInsights
         });
     }
--- scripts/syncProblems.js
+++ scripts/syncProblems.js
@@ -4,11 +4,15 @@ import pool from "../db/connection.js";
 try{
     const res = await axios.get("https://codeforces.com/api/problemset.problems");
     const problems = res.data.result.problems;
-
+    console.log(`Fetched ${problems.length} problems
+        `)
     await pool.query("BEGIN");
     await pool.query("TRUNCATE TABLE problems");
     
+    let count = 0;
     for(const problem of problems){
+        
+
         await pool.query(`
         INSERT INTO problems (contest_id, problem_index, name, rating, tags)
         VALUES ($1, $2, $3, $4, $5)`, [
@@ -18,8 +22,16 @@ try{
             problem.rating ?? null,
             problem.tags
         ])
+
+        count++;
+
+        if (count % 100 === 0) {
+            console.log(`Inserted ${count}`);
+        }
     }
+    console.log("Committing...")
     await pool.query("COMMIT");
+    console.log("Done!")
 }
 catch(err){
     await pool.query("ROLLBACK");
